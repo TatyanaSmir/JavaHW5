28d7508e5f92138714e4b609f9736ed797a57b82
TatyanaSmir <tatyanasmirnova404@gmail.com>
2022-07-01 18:09:44 +0300

    remote  src/main/java/Main.java

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import ru.netology.sqr.SQRService;
-
-public class Main {
-    public static void main(String[] args) {
-        SQRService service = new SQRService();
-        System.out.println("Попадание в диапазон " + service.calcSqrOfNumber( 200, 300) + " раз(а)");
-    }
-}
